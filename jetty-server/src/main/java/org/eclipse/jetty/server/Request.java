4edb965b55b930a2b3d8aae9251483bc5eb1b928
olivier lamy <oliver.lamy@gmail.com>
2019-05-14 18:01:10 +1000

    tck can use a ServletRequestWrapper/ServletResponseWrapper so direct cast is not possible
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/Request.java
@@ -2273,7 +2273,7 @@ public class Request implements HttpServletRequest
         AsyncContextEvent event = new AsyncContextEvent(_context,_async,state,this,servletRequest,servletResponse);
         event.setDispatchContext(getServletContext());
         
-        String uri = ((HttpServletRequest)servletRequest).getRequestURI();
+        String uri = getRequestURI(servletRequest);// ((HttpServletRequest)servletRequest).getRequestURI();
         if (_contextPath!=null && uri.startsWith(_contextPath))
             uri = uri.substring(_contextPath.length());
         else
@@ -2283,6 +2283,13 @@ public class Request implements HttpServletRequest
         event.setDispatchPath(uri);
         state.startAsync(event);
         return _async;
+    }
+
+    protected String getRequestURI(ServletRequest servletRequest){
+        if(servletRequest instanceof ServletRequestWrapper){
+            return ((HttpServletRequest)((ServletRequestWrapper)servletRequest).getRequest()).getRequestURI();
+        }
+        return ((HttpServletRequest)servletRequest).getRequestURI();
     }
 
     /* ------------------------------------------------------------ */
